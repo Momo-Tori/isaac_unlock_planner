d5028b0f13c20d283a290bcbcd0a1c7e7b086016
涛 杨 <813424850@qq.com>
2026-08-26 01:04:43 +0800

    feat: 添加推荐不解锁

--- data/recommendation_profiles.js
+++ data/recommendation_profiles.js
@@ -392,6 +392,11 @@ window.ISAAC_RECOMMENDATION_PROFILES = {
         },
         {
           "characterId": "c12-keeper",
+          "bossId": "boss-rush",
+          "priority": "discouraged"
+        },
+        {
+          "characterId": "c12-keeper",
           "bossId": "hush",
           "priority": "recommended"
         },
@@ -877,6 +882,11 @@ window.ISAAC_RECOMMENDATION_PROFILES = {
         },
         {
           "characterId": "c26-tainted-eden",
+          "bossId": "beast",
+          "priority": "discouraged"
+        },
+        {
+          "characterId": "c26-tainted-eden",
           "bossId": "greedier",
           "priority": "strong"
         },
@@ -889,6 +899,11 @@ window.ISAAC_RECOMMENDATION_PROFILES = {
           "characterId": "c27-tainted-lost",
           "bossId": "hush",
           "priority": "recommended"
+        },
+        {
+          "characterId": "c27-tainted-lost",
+          "bossId": "mega-satan",
+          "priority": "discouraged"
         },
         {
           "characterId": "c27-tainted-lost",
